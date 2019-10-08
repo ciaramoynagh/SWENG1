c595d5a17c84c1aa06b2e13dc94d38661b6dfb99
ciaramoynagh <47217285+ciaramoynagh@users.noreply.github.com>
2019-10-08 15:14:34 +0100

    added tests

--- src/LCATest.java
+++ src/LCATest.java
@@ -6,6 +6,31 @@ public class LCATest {
 	@Test
 	public void testEmpty() {
 		LCA.BT tree = new LCA.BT();
+		
+		assertEquals(tree.findLCA(1,4), -1);
+		assertEquals(tree.findLCA(2,3), -1);
+		
 	}
-
+	
+	@Test
+	public void testTwoNodes() {
+		LCA.BT tree = new LCA.BT();
+		
+		tree.root = new LCA.Node(1);
+		tree.root.left = new LCA.Node(2);
+		
+		assertEquals(tree.findLCA(1,2), 1);
+	}	
+	
+	@Test
+	public void testThreeNodes() {
+		LCA.BT tree = new LCA.BT();
+		
+		tree.root = new LCA.Node(1);
+		tree.root.left = new LCA.Node(2);
+		tree.root.right = new LCA.Node(3);
+		
+		assertEquals(tree.findLCA(2,3), 1);
+	}
 }
+
